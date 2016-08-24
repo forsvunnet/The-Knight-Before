c71fa734844214af7b25a72211f5cb93684bf168
Eivin Landa <landa@drivdigital.no>
2016-08-24 18:29:20 +0200

    Restructured all the stuff a bit

---
+++ The Knight Before/Assets/scripts/God_Script.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+var knight : Transform;
+var background : Transform;
+var ground : Transform;
+function Start () {
+
+	// Create the knight
+	Instantiate(
+		knight,
+		new Vector3(0, 0, 0),
+		Quaternion.identity
+	);
+
+	// Create the first ground and the first landscape
+	Instantiate(
+		ground,
+		new Vector3(0, 0, 0),
+		Quaternion.identity
+	);
+	Instantiate(
+		background,
+		new Vector3(0, 0, 0),
+		Quaternion.identity
+	);
+}
+
+function Update () {
+
+}
