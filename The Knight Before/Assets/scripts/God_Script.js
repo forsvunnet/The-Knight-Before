9ca0da3ea159f2fe1f8e2235ea848378d095b139
Eivin Landa <landa@drivdigital.no>
2016-08-24 20:37:38 +0200

    More stuff

--- The Knight Before/Assets/scripts/God_Script.js
+++ The Knight Before/Assets/scripts/God_Script.js
@@ -2,28 +2,52 @@
 var knight : Transform;
 var background : Transform;
 var ground : Transform;
+
+private var last_x = 0;
+private var _knight : Transform;
 function Start () {
 
 	// Create the knight
-	Instantiate(
+	_knight = Instantiate(
 		knight,
 		new Vector3(0, 0, 0),
 		Quaternion.identity
 	);
 
+	// set up the camera to follow the knight
+	var camera  = GameObject.Find('Main Camera');
+	var follower = camera.GetComponent( UnityStandardAssets._2D.Camera2DFollow );
+	follower.target = _knight;
+
 	// Create the first ground and the first landscape
 	Instantiate(
 		ground,
-		new Vector3(0, 0, 0),
+		new Vector3(last_x, -3.5, 0),
 		Quaternion.identity
 	);
+
+
 	Instantiate(
 		background,
-		new Vector3(0, 0, 0),
+		new Vector3(-500, -10, 0),
 		Quaternion.identity
 	);
+	Debug.Log( _knight.position );
+	Debug.Log( _knight );
+	Debug.Log( last_x + 20 );
 }
 
 function Update () {
+	if ( last_x < _knight.position.x ) {
+		//Debug.Log( _knight.position.x );
+		last_x += 20;
+		Instantiate(
+			ground,
+			new Vector3(last_x, -3.5, 0),
+			Quaternion.identity
+		);
+	}
+
+//	Time.deltaTime
 
 }
--- The Knight Before/Assets/scripts/Running.js
+++ The Knight Before/Assets/scripts/Running.js
@@ -1,9 +1,18 @@
 ﻿#pragma strict
-var speed = 1;
+var max_speed = 15;
+var speed = 1.00;
+var x_velocity = 0.00;
+var use_velocity = true;
 function Start () {
 
 }
 
 function Update () {
-	transform.Translate( speed * Vector3.right * Time.deltaTime);
+	if ( Time.time < 3 ) {
+		return;
+	}
+	if ( x_velocity < max_speed ) {
+		x_velocity += Time.deltaTime * speed;
+	}
+	transform.Translate( x_velocity * Vector3.right * Time.deltaTime);
 }
